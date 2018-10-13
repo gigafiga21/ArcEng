f9031871f22e1afbdc82132c3045ff97431fd91e
Илья Колдунов <gigafiga21@ECHO.hp620>
2018-10-13 18:43:14 +0300

    Created inners and outer drawing alghorythm (issue #3).

--- Libraries/E_Canvas/Definition.cpp
+++ Libraries/E_Canvas/Definition.cpp
@@ -1,27 +1,83 @@
-typedef struct
-{
-    int x, y;
-} Point;
+#include <iostream>
 
 class E_Canvas : public Fl_Widget
 {
     private:
 
-        int iStrokeStep, iLineWeight, iHalfWallWeight,
+        int iStrokeStep, iLineWeight, iWallWeight,
             iLeft, iTop, iWidth, iHeight,
             iCanvasLeft, iCanvasTop, iCanvasWidth, iCanvasHeight;
-        std::vector<Point> aBoundaryLines;
 
-        void strokeRect(int iLeft, int iTop, int iWidth, int iHeight)
+        Polygon plgFlat;
+
+        void strokeFlat()
         {
-            fl_push_clip(iLeft, iTop, iWidth, iHeight);
+            fl_push_clip(iCanvasLeft, iCanvasTop, iCanvasWidth, iCanvasHeight);
 
-            for (int iCounter = 0; iCounter < iWidth + iHeight; iCounter += iStrokeStep)
+            for (int iCounter = 0; iCounter < iCanvasWidth + iCanvasHeight; iCounter += iStrokeStep)
             {
-                fl_line(iLeft + iCounter, iTop, iLeft, iTop + iCounter);
+                fl_line(iCanvasLeft + iCounter, iCanvasTop, iCanvasLeft, iCanvasTop + iCounter);
             }
 
             fl_pop_clip();
+        }
+
+        void drawFlatOuter()
+        {
+            std::vector<Point> vptFlatOuter = plgFlat.outer();
+
+            fl_color(E_COLOR1);
+            fl_begin_polygon();
+            fl_vertex(iCanvasLeft - 1, iCanvasTop - 1);
+            fl_vertex(iCanvasLeft + iCanvasWidth + 1, iCanvasTop - 1);
+            fl_vertex(iCanvasLeft + iCanvasWidth + 1, iCanvasTop + iCanvasHeight + 1);
+            fl_vertex(iCanvasLeft - 1, iCanvasTop + iCanvasHeight + 1);
+            fl_vertex(iCanvasLeft - 1, iCanvasTop - 1);
+            fl_gap();
+
+            for (int iCounter = 0; iCounter < vptFlatOuter.size(); iCounter++)
+            {
+                fl_vertex(vptFlatOuter[iCounter].x() + iCanvasLeft, vptFlatOuter[iCounter].y() + iCanvasTop);
+            }
+
+            fl_end_polygon();
+
+            fl_color(E_COLOR3);
+            fl_begin_loop();
+            fl_vertex(iCanvasLeft, iCanvasTop);
+            fl_vertex(iCanvasLeft + iCanvasWidth, iCanvasTop);
+            fl_vertex(iCanvasLeft + iCanvasWidth, iCanvasTop + iCanvasHeight);
+            fl_vertex(iCanvasLeft, iCanvasTop + iCanvasHeight);
+            fl_vertex(iCanvasLeft, iCanvasTop);
+            fl_end_loop();
+        }
+
+        void pushFlatInnersVerteces()
+        {
+            for (int iInnerIndex = 0; iInnerIndex < plgFlat.inners().size(); iInnerIndex++)
+            {
+                std::vector<Point> vptFlatInner = plgFlat.inners()[iInnerIndex];
+
+                for (int iCounter = 0; iCounter < vptFlatInner.size(); iCounter++)
+                {
+                    fl_vertex(vptFlatInner[iCounter].x() + iCanvasLeft, vptFlatInner[iCounter].y() + iCanvasTop);
+                }
+
+                fl_gap();
+            }
+        }
+
+        void drawFlatInners()
+        {
+            fl_color(E_COLOR1);
+            fl_begin_polygon();
+            pushFlatInnersVerteces();
+            fl_end_polygon();
+
+            fl_color(E_COLOR3);
+            fl_begin_loop();
+            pushFlatInnersVerteces();
+            fl_end_loop();
         }
 
     public:
@@ -35,7 +91,7 @@ class E_Canvas : public Fl_Widget
         {
             iStrokeStep = 5;
             iLineWeight = 2;
-            iHalfWallWeight = 5;
+            iWallWeight = 8;
 
             iLeft = iGivenLeft + iLineWeight;
             iTop = iGivenTop + iLineWeight;
@@ -47,16 +103,27 @@ class E_Canvas : public Fl_Widget
             iCanvasWidth = iWidth - 60;
             iCanvasHeight = 265;
 
-            aBoundaryLines.push_back({iHalfWallWeight, iHalfWallWeight});
-            aBoundaryLines.push_back({iCanvasWidth - 2 * iHalfWallWeight, iHalfWallWeight});
-            aBoundaryLines.push_back({iCanvasWidth - 2 * iHalfWallWeight, iCanvasHeight - 2 * iHalfWallWeight});
-            aBoundaryLines.push_back({iHalfWallWeight, iCanvasHeight - 2 * iHalfWallWeight});
+            BG::append(plgFlat.outer(), Point(0, 0));
+            BG::append(plgFlat.outer(), Point(iCanvasWidth, 0));
+            BG::append(plgFlat.outer(), Point(iCanvasWidth, iCanvasHeight));
+            BG::append(plgFlat.outer(), Point(0, iCanvasHeight));
+            BG::append(plgFlat.outer(), Point(0, 0));
+
+            plgFlat.inners().resize(1);
+            BG::append(plgFlat.inners()[0], Point(iWallWeight, iWallWeight));
+            BG::append(plgFlat.inners()[0], Point(iCanvasWidth - iWallWeight, iWallWeight));
+            BG::append(plgFlat.inners()[0], Point(iCanvasWidth - iWallWeight, iCanvasHeight - iWallWeight));
+            BG::append(plgFlat.inners()[0], Point(iWallWeight, iCanvasHeight - iWallWeight));
+            BG::append(plgFlat.inners()[0], Point(iWallWeight, iWallWeight));
         }
 
         void draw()
         {
-            fl_color(E_COLOR3);
-            fl_line_style(FL_SOLID, iHalfWallWeight, NULL);
+            fl_line_style(FL_SOLID, iLineWeight, NULL);
 
+            fl_color(E_COLOR3);
+            strokeFlat();
+            drawFlatOuter();
+            drawFlatInners();
         }
 };
--- Libraries/E_Canvas/Mask.h
+++ Libraries/E_Canvas/Mask.h
@@ -2,8 +2,13 @@
 
     #define E_CANVAS : 1
     #include <vector>
+    #include <boost/geometry.hpp>
     #include <FL/fl_draw.H>
 
+    namespace BG = boost::geometry;
+
+    typedef BG::model::polygon<boost::geometry::model::d2::point_xy<int>> Polygon;
+    typedef BG::model::d2::point_xy<int> Point;
 
 
     #include "Definition.cpp"
